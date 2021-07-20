1e81e67070d30cebb7074828d652e084be6a588e
GeorgGoldbach <87702150+GeorgGoldbach@users.noreply.github.com>
2021-07-20 13:20:57 +0200

    Add Zinsgrabber.py
    This python file downloads the discount rate files (.png and .pdf) from
    the acutaries Mercer, AON, KMKOLL, WTW and Heubeck from their websites.

---
+++ Zinsgrabber.py
@@ -0,0 +1,196 @@
+#!/usr/bin/python3
+import subprocess
+import os.path
+from datetime import datetime
+import re
+
+def search_string_in_file(filename, searchstring):
+    """Search for given string in file and return lines containing that string
+    (source: https://thispointer.com/python-search-strings-in-a-file-and-get-line-numbers-of-lines-containing-the-string/)"""
+    linenumber = 0
+    resultlist = []
+    # Opem file in read only mode
+    with open(filename, 'r') as file:
+        # Real all line in the file one by one
+        for line in file:
+            # For each line, check if line contains the strin
+            linenumber += 1
+            if searchstring in line:
+                # If yes, then add the line in the list
+                resultlist.append(line.rstrip())
+    # Return list of tuples containing line numbers and lines where string is found
+    return resultlist
+
+
+# Mercer
+# ======
+
+for actuary in ["Mercer", "AON", "KMKOLL", "WTW", "Heubeck"]:
+
+    if actuary == "Mercer":
+        actuaryurl = 'https://www.mercer.de/our-thinking/rechnungszins-fuer-ifrs-us-gaap-bilmog-bewertungen.html'
+
+        # Indicators for reportdate
+        standindicator = '(Stand '
+        cutstandindicatorleft = '(Stand '
+
+        # Indicators for fileurl
+        fileurlbeginning = 'https://www.mercer.de'
+        fileurlindicator = 'src="/content/dam/mercer/assets/content-images/'
+        cutindicatorleft = 'src="'
+        cutindicatorright = '.png'
+
+    elif actuary == "AON":
+        actuaryurl = 'https://www.aon.com/germany/publikationen/human-resources/rechnungszins.jsp'
+
+        # Indicators for reportdate
+        standindicator = '(Stand '
+        cutstandindicatorleft = '(Stand '
+
+        # Indicators for fileurl
+        fileurlbeginning = 'https://www.aon.com'
+        fileurlindicator = 'Rechnungszinsinformation'
+        cutindicatorleft = 'href="'
+        cutindicatorright = '.pdf'
+
+    elif actuary == "KMKOLL":
+        actuaryurl = 'https://www.kmkoll.de/IFRS-Zinsermittlung.aspx'
+
+        # Indicators for reportdate
+        standindicator = 'Stichtag">'
+        cutstandindicatorleft = 'Stichtag">'
+
+        # Indicators for fileurl
+        fileurlbeginning = 'https://www.kmkoll.de/'
+        fileurlindicator = '.pdf'
+        cutindicatorleft = 'href="'
+        cutindicatorright = '.pdf'
+
+    elif actuary == "WTW":
+        actuaryurl = 'https://www.willistowerswatson.com/de-DE/Insights/campaigns/rechnungszins-nach-ifrs-us-gaap-und-hgb'
+
+        # Indicators for reportdate
+        standindicator = '/Rechnungszins/Rechnungszins-'
+        cutstandindicatorleft = '/Rechnungszins/Rechnungszins-'
+
+        # Indicators for fileurl
+        fileurlbeginning = ''
+        fileurlindicator = '/Rechnungszins/Rechnungszins-'
+        cutindicatorleft = 'href="'
+        cutindicatorright = '.pdf'
+
+    elif actuary == "Heubeck":
+        actuaryurl = 'https://www.heubeck.de/aktuelles/informationen/zinsinfo'
+
+        # Indicators for reportdate
+        standindicator = ''
+        cutstandindicatorleft = ''
+
+        # Indicators for fileurl
+        fileurlbeginning = 'https://www.heubeck.de/assets/Download/HI_Zinsinfo/'
+        fileurlindicator = '.pdf'
+        cutindicatorleft = 'href="/assets/Download/HI_Zinsinfo/'
+        cutindicatorright = '.pdf'
+
+    # START
+    print(actuary)
+    print("=" * len(actuary))
+
+    actuaryfile = f"{actuary}.txt"
+    subprocess.run(["wget", "--no-check-certificate", actuaryurl, "-O", actuaryfile], stderr=subprocess.DEVNULL)
+
+    # Search for reportdate 
+    searchstand = search_string_in_file(actuaryfile, standindicator)
+
+    # Search for fileurl
+    if actuary == "Heubeck":
+        searchfileurl = []
+        results = search_string_in_file(actuaryfile, fileurlindicator)
+        for result in results:
+            for href in result.split(cutindicatorleft)[1:]:
+                href = href.replace(".PDF", ".pdf")
+                href = href.split(cutindicatorright, 1)[0] + cutindicatorright
+                if "IFRS" in href:
+                    searchfileurl.append(cutindicatorleft + href)
+    else:
+        searchfileurl = search_string_in_file(actuaryfile, fileurlindicator)
+
+    i = 0
+    for line in searchfileurl:
+
+        if actuary in ["Mercer", "KMKOLL"]:
+            stand = searchstand[0].split(cutstandindicatorleft)[1]
+            year = stand.split('.')[2][0:4]
+            month = stand.split('.')[1]
+            day = stand.split('.')[0]
+            reportdate = f"{year}-{month}-{day}"
+        elif actuary == "AON":
+            stand = searchstand[i].split(cutstandindicatorleft)[1]
+            year = stand.split('.')[2][0:4]
+            month = stand.split('.')[1]
+            day = stand.split('.')[0]
+            reportdate = f"{year}-{month}-{day}"
+        elif actuary == "WTW":
+            stand = searchstand[i].split(cutstandindicatorleft)[1]
+            year = stand.split('-')[1][0:4]
+            month = stand.split('-')[0]
+            if month == "Januar":
+                month = "01"
+            elif month == "Februar":
+                month = "02"
+            elif month in ["Mrz", "Maerz"]:
+                month = "03"
+            elif month == "April":
+                month = "04"
+            elif month == "Mai":
+                month = "05"
+            elif month == "Juni":
+                month = "06"
+            elif month == "Juli":
+                month = "07"
+            elif month == "August":
+                month = "08"
+            elif month == "September":
+                month = "09"
+            elif month == "Oktober":
+                month = "10"
+            elif month == "November":
+                month = "11"
+            elif month == "Dezember":
+                month = "12"
+            else:
+                month = "13"
+            reportdate = f"{year}-{month}"
+        elif actuary == "Heubeck":
+            reportdate = re.search(r'\d+-\d+-\d+', line).group()
+            year = reportdate.split('-')[0][0:4]
+            month = reportdate.split('-')[1]
+            day = reportdate.split('-')[2]
+            reportdate = f"{year}-{month}-{day}"
+
+        # Create dir for reportmonth
+        reportmonthdir = f"{year}-{month}"
+        if not os.path.exists(reportmonthdir):
+            os.mkdir(reportmonthdir)
+
+        line = line.split(cutindicatorleft)[1]
+        fileurlending = line.split(cutindicatorright, 1)[0] + cutindicatorright
+
+        fileurl = fileurlbeginning + fileurlending
+        filename = fileurl.split('/')[-1]
+        fileout = f"{reportmonthdir}/{actuary}-{reportdate}_{filename}"
+
+        # download files
+        if os.path.isfile(fileout):
+            print(f"File already exists: {fileout}")
+        else:
+            subprocess.run(["wget", "--no-check-certificate", fileurl, "-O", fileout], stderr=subprocess.DEVNULL)
+            print(f"File downloaded: {fileout}")
+
+            zinsurls = open(f"{reportmonthdir}/ZinsURLs-{year}-{month}.txt", mode='a')
+            zinsurls.write(fileurl + "\n")
+            zinsurls.close()
+
+        i += 1
+
+    os.remove(actuaryfile)
